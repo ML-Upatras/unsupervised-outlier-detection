dbc88442e7e576d23c0bf958f9607096842d8472
Pantelis Linardatos <pant.linardatos@gmail.com>
2021-01-19 07:59:23 +0200

    Modify benchmarking.py

--- benchmarking.py
+++ benchmarking.py
@@ -1,75 +1,49 @@
-from __future__ import division
-from __future__ import print_function
-
 import os
-import sys
-from time import time
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname("__file__"), '..')))
-
-import warnings
-warnings.filterwarnings("ignore")
-
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score
+from time import time
 from scipy.io import loadmat
 
-from pyod.models.abod import ABOD
-from pyod.models.cblof import CBLOF
-from pyod.models.feature_bagging import FeatureBagging
-from pyod.models.hbos import HBOS
-from pyod.models.iforest import IForest
-from pyod.models.knn import KNN
-from pyod.models.lof import LOF
-from pyod.models.mcd import MCD
-from pyod.models.ocsvm import OCSVM
-from pyod.models.pca import PCA
-from pyod.models.combination import aom, moa, average, maximization
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+
 from pyod.utils.utility import standardizer
 from pyod.utils.utility import precision_n_scores
 
-from fsfc.generic import Lasso, LFSBSS
-from fsfc.generic import NormalizedCut, GenericSPEC, FixedSPEC, SPEC
-from fsfc.generic import MCFS
-from fsfc.generic import WKMeans
-
-from stac.nonparametric_tests import friedman_test 
+from core.utils import define_classifiers, define_normalizers, define_combination_methods
+from pyod.models.combination import aom, moa, average, maximization
 
 
-# Define data file and read X and y
+# define data file and read X and y
 mat_file_list = ['arrhythmia.mat',
                  'cardio.mat',
                  'glass.mat',
                  'ionosphere.mat',
                  'letter.mat',
                  'lympho.mat',
-                #  'mnist.mat',
+                 'mnist.mat',
                  'musk.mat',
                  'optdigits.mat',
                  'pendigits.mat',
                  'pima.mat',
                  'satellite.mat',
                  'satimage-2.mat',
-                #  'shuttle.mat',
+                 'shuttle.mat',
                  'vertebral.mat',
-                #  'vowels.mat',
+                 'vowels.mat',
                  'wbc.mat'
                  ]
 
-# Define nine outlier detection tools to be compared
+# define nine outlier detection tools to be compared
 random_state = np.random.RandomState(42)
 
-df_columns = ['Data', '#Samples', '# Dimensions', 'Outlier Perc',
-              'ABOD', 'CBLOF', 'FB', 'HBOS', 'IForest', 'KNN', 'LOF', 'MCD',
-              'OCSVM', 'PCA']
-roc_df = pd.DataFrame(columns=df_columns)
-prn_df = pd.DataFrame(columns=df_columns)
-time_df = pd.DataFrame(columns=df_columns)
+# define containers for results of all datasets
+method_names = []
+roc_results = {}
+prn_results = {}
 
-first = True
-for mat_file in mat_file_list:
-    print("\n... Processing", mat_file, '...')
+for num_mat, mat_file in enumerate(mat_file_list):
+    print("\n... Processing", mat_file, '...', '\n')
     mat = loadmat(os.path.join('data', mat_file))
 
     X = mat['X']
@@ -77,158 +51,139 @@ for mat_file in mat_file_list:
     outliers_fraction = np.count_nonzero(y) / len(y)
     outliers_percentage = round(outliers_fraction * 100, ndigits=4)
 
-    # construct containers for saving results
-    roc_list = [mat_file[:-4], X.shape[0], X.shape[1], outliers_percentage]
-    prn_list = [mat_file[:-4], X.shape[0], X.shape[1], outliers_percentage]
-    time_list = [mat_file[:-4], X.shape[0], X.shape[1], outliers_percentage]
+    print ('Dataset Shape:', X.shape)
+    print ('Outliers Percentage', outliers_percentage)
+
+    # construct containers for saving results of each dataset
+    roc_list = []
+    prn_list = []
+    time_list = []
 
     # 60% data for training and 40% for testing
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4,
                                                         random_state=random_state)
     
-    print (X_train.shape)
     # standardizing data for processing
     X_train_norm, X_test_norm = standardizer(X_train, X_test)
 
-    classifiers = {'Angle-based Outlier Detector (ABOD)': ABOD(
-        contamination=outliers_fraction),
-        'Cluster-based Local Outlier Factor': CBLOF(
-            contamination=outliers_fraction, check_estimator=False,
-            random_state=random_state),
-        'Feature Bagging': FeatureBagging(contamination=outliers_fraction,
-                                          random_state=random_state),
-        'Histogram-base Outlier Detection (HBOS)': HBOS(
-            contamination=outliers_fraction),
-        'Isolation Forest': IForest(contamination=outliers_fraction,
-                                    random_state=random_state),
-        'K Nearest Neighbors (KNN)': KNN(contamination=outliers_fraction),
-        'Local Outlier Factor (LOF)': LOF(
-            contamination=outliers_fraction),
-        'Minimum Covariance Determinant (MCD)': MCD(
-            contamination=outliers_fraction, random_state=random_state),
-        'One-class SVM (OCSVM)': OCSVM(contamination=outliers_fraction),
-        'Principal Component Analysis (PCA)': PCA(
-            contamination=outliers_fraction, random_state=random_state)
-    }
-    
+    # define classifiers
+    classifiers = define_classifiers(random_state, outliers_fraction)
+
+    # create df for results
     results = pd.DataFrame(columns=classifiers.keys())
-    
+
+    print ('\n', 'Outliers Detection', '\n')
     for clf_name, clf in classifiers.items():
+
+        # keep name of convetional models (once on the first itteration)
+        if num_mat == 0:
+            method_names.append(clf_name)
+
+        # train
         t0 = time()
         clf.fit(X_train_norm)
         test_scores = clf.decision_function(X_test_norm)
+        # Then the output scores are standardized into zero average and unit std before combination. 
+        # This step is crucial to adjust the detector outputs to the same scale.
         standarized_scores = standardizer(test_scores.reshape(-1, 1))
         results[clf_name] = standarized_scores.reshape(1,-1)[0]
         t1 = time()
+
+        #calculate metrics of interest
         duration = round(t1 - t0, ndigits=4)
-        time_list.append(duration)
-        
         roc = round(roc_auc_score(y_test, test_scores), ndigits=4)
         prn = round(precision_n_scores(y_test, test_scores), ndigits=4)
 
         print('{clf_name} ROC:{roc}, precision @ rank n:{prn}, '
               'execution time: {duration}s'.format(
             clf_name=clf_name, roc=roc, prn=prn, duration=duration))
-
+            
+        time_list.append(duration)
         roc_list.append(roc)
         prn_list.append(prn)
+
+    # total duration of outlier detection methods to run
+    duration_until_now = round(np.sum(time_list[4:]), ndigits=4)
+    print ('\n', 'Duration of outliers detection', duration_until_now, '\n')
+
+    k= 6 # From the 10 algorithms, 4 are discarded (the best are keeped for stacking). 
+    normalizers = define_normalizers(k)
     
-    k= 6 # From the 10 algorithms, 2 are discarded. 
-    normalizers ={
-        # "Fixed SPEC" : FixedSPEC(k, 2),
-        "Generic SPEC" : GenericSPEC(k),
-        "NormalizedCut" : NormalizedCut(k),
-        "Lasso" : Lasso(k, 0.01),
-        # "LFSBSS" : LFSBSS(k),
-        # "MCFS" : MCFS(k, 2),
-        "WKMeans" : WKMeans(k, 0.1)
-
-    }
-
+    print ('\n', 'Unsupervised Feature Selection & Stacking', '\n')
     for normalizer_name, normalizer in normalizers.items():
-
+        t0 = time()        
         norm_results = normalizer.fit_transform(results.values)
+        t1 = time()
+        duration = duration_until_now + round(t1 - t0, ndigits=4)
 
-        combination_methods = {
-            normalizer_name+'_average_'+str(k): average(norm_results),
-            normalizer_name+'_maximization_'+str(k) : maximization(norm_results),
-            normalizer_name+'_aom_'+str(k) : aom(norm_results, int(k/2)),
-            normalizer_name+'_moa_'+str(k) : moa(norm_results, int(k/2))
-        }
-
+        # define combination methods
+        combination_methods = define_combination_methods(normalizer_name, k, norm_results)
         for cmb_name, cmb in combination_methods.items():
+
+            #keep name of combination (once on the first itteration)
+            if num_mat == 0:
+                method_names.append(cmb_name)
 
             roc = round(roc_auc_score(y_test, cmb), ndigits=4)
             prn = round(precision_n_scores(y_test, cmb), ndigits=4)
 
             roc_list.append(roc)
             prn_list.append(prn)
-            time_list.append(np.sum(time_list[4:14]))
+            time_list.append(duration)
 
-            if first:
-                df_columns.append(cmb_name)
-    
-    first = False
-    temp_df = pd.DataFrame(time_list).transpose()
-    temp_df.columns = df_columns
-    time_df = pd.concat([time_df, temp_df], axis=0)
+            print('{cmb_name} ROC:{roc}, precision @ rank n:{prn}, '
+              'execution time: {duration}s'.format(
+            cmb_name=cmb_name, roc=roc, prn=prn, duration=duration))
 
-    temp_df = pd.DataFrame(roc_list).transpose()
-    temp_df.columns = df_columns
-    roc_df = pd.concat([roc_df, temp_df], axis=0)
+    roc_results[num_mat] = [-1 * x for x in roc_list]
+    prn_results[num_mat] = [-1 * x for x in prn_list]
 
-    temp_df = pd.DataFrame(prn_list).transpose()
-    temp_df.columns = df_columns
-    prn_df = pd.concat([prn_df, temp_df], axis=0)
+print ('\n Number of method were used: {}'.format(len(method_names), ))
 
+# Export all results
+roc_df = pd.DataFrame(roc_results).transpose()
+roc_df.columns=method_names
+roc_df.to_csv('results/final_roc.csv', index=False)
 
-roc_df.to_excel('results/roc_performance.xlsx')
-print('ROC Performance Exported')
+prn_df = pd.DataFrame(prn_results).transpose()
+prn_df.columns=method_names
+prn_df.to_csv('results/final_prn.csv', index=False)
 
-prn_df.to_excel('results/precision_performance.xlsx')
-print('Precision Performance Exported')
+# Export results for comparison between the algorithms and the proposed technique
+roc_comparison_df = roc_df[['Angle-based Outlier Detector (ABOD)', 'Cluster-based Local Outlier Factor', 'Feature Bagging',
+                            'Histogram-base Outlier Detection (HBOS)', 'Isolation Forest', 'K Nearest Neighbors (KNN)',
+                            'Local Outlier Factor (LOF)', 'Minimum Covariance Determinant (MCD)','One-class SVM (OCSVM)',
+                            'Principal Component Analysis (PCA)', 'Lasso Moa_6']]
+prn_comparison_df = prn_df[['Angle-based Outlier Detector (ABOD)', 'Cluster-based Local Outlier Factor', 'Feature Bagging',
+                            'Histogram-base Outlier Detection (HBOS)', 'Isolation Forest', 'K Nearest Neighbors (KNN)',
+                            'Local Outlier Factor (LOF)', 'Minimum Covariance Determinant (MCD)','One-class SVM (OCSVM)',
+                            'Principal Component Analysis (PCA)', 'Lasso Moa_6']]
 
-#Calculate Error ROC
-results_roc = roc_df.loc[:, 'ABOD' : ]
-roc_error = 1 - results_roc
-roc_error.index = range (0, len(mat_file_list)) 
+roc_comparison_df = roc_comparison_df.copy()
+prn_comparison_df = prn_comparison_df.copy()
 
-#Friedman Ranking ROC
-statistic, p_value, ranking, rank_cmp = friedman_test(*roc_error.to_dict().values())
-friedman = pd.DataFrame(index = results_roc.columns.tolist())
-friedman['ranking'] = ranking
-friedman.sort_values(by='ranking').to_excel('results/friedman_roc.xlsx')
-print('Friedman Ranking ROC Exported')
+roc_comparison_df['Moa'] = moa(roc_comparison_df.iloc[:, :10], 5)
+prn_comparison_df['Moa'] = moa(prn_comparison_df.iloc[:, :10], 5)
 
-# Top 5 ROC Performing Algorithms
-temp = roc_df.loc[:, 'ABOD' : ].astype('float32').apply(lambda s: s.abs().nlargest(5).index.tolist(), axis=1)
-roc_top5 = pd.DataFrame(columns=['First', 'Second', 'Third', 'Fourth', 'Fifth'], index=range(len(temp)))
-for i in range(len(temp)):
-    roc_top5.iloc[i] = temp.iloc[i]
-    
-roc_top5.index = mat_file_list
-roc_top5.to_excel('results/roc_top_5.xlsx')
-print('ROC Performance Top 5 Exported')
+roc_comparison_df.to_csv('results/roc_comparison.csv', index=False)
+prn_comparison_df.to_csv('results/prn_comparison.csv', index=False)
 
-#Calculate Error Precision
-results_prn = prn_df.loc[:, 'ABOD' : ]
-prn_error = 1 - results_prn
-prn_error.index = range (0, len(mat_file_list)) 
+# Export results for comparison between different choices of feature selection
+roc_fs_cols = [col for col in roc_df.columns if 'Moa' in col]
+prn_fs_cols = [col for col in prn_df.columns if 'Moa' in col]
 
-#Friedman Ranking Precision
-statistic, p_value, ranking, rank_cmp = friedman_test(*prn_error.to_dict().values())
-friedman = pd.DataFrame(index = results_prn.columns.tolist())
-friedman['ranking'] = ranking
-friedman.sort_values(by='ranking').to_excel('results/friedman_prn.xlsx')
-friedman.sort_values(by='ranking')
-print('Friedman Ranking Precision Exported')
+roc_comparison_2_df = roc_df[roc_fs_cols]
+prn_comparison_2_df = roc_df[prn_fs_cols]
 
-# Top 5 Precision Performing Algorithms
-temp = prn_df.loc[:, 'ABOD' : ].astype('float32').apply(lambda s: s.abs().nlargest(5).index.tolist(), axis=1)
-prn_top5 = pd.DataFrame(columns=['First', 'Second', 'Third', 'Fourth', 'Fifth'], index=range(len(temp)))
-for i in range(len(temp)):
-    prn_top5.iloc[i] = temp.iloc[i]
+roc_comparison_2_df.to_csv('results/roc_comparison_2.csv', index=False)
+prn_comparison_2_df.to_csv('results/prn_comparison_2.csv', index=False)
 
-prn_top5.index = mat_file_list
-prn_top5.to_excel('results/prn_top_5.xlsx')
-print('Precision Performance Top 5 Exported')
+# Export results for comparison between different choices of aggregation techniques 
+roc_agg_cols = [col for col in roc_df.columns if 'Lasso' in col]
+prn_agg_cols = [col for col in prn_df.columns if 'Lasso' in col]
+
+roc_comparison_3_df = roc_df[roc_agg_cols]
+prn_comparison_3_df = roc_df[prn_agg_cols]
+
+roc_comparison_3_df.to_csv('results/roc_comparison_3.csv', index=False)
+prn_comparison_3_df.to_csv('results/prn_comparison_3.csv', index=False)
